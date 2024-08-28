8eb22bea8660af5eb13c6105ff38a442228befee
ValeMP12 <preciadovaleriam@gmail.com>
2024-08-27 18:46:55 -0600

    fix(style): updatings variables tailwind

--- frontend/src/styles/font.ts
+++ frontend/src/styles/font.ts
@@ -52,4 +52,50 @@ export const montserratConfig: LocalFontOptions = {
   display: 'swap',
 };
 
-export const montserrat = localFont(montserratConfig);
+export const montserrat = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Montserrat-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Montserrat-RegularItalic.ttf',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/Montserrat-Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Montserrat-MediumItalic.ttf',
+      weight: '500',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/Montserrat-SemiBold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Montserrat-SemiBoldItalic.ttf',
+      weight: '600',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/Montserrat-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Montserrat-BoldItalic.ttf',
+      weight: '700',
+      style: 'italic',
+    },
+  ],
+  variable: '--font-montserrat',
+  fallback: ['sans-serif', 'system-ui', 'Apple Color Emoji'],
+  display: 'swap',
+});
