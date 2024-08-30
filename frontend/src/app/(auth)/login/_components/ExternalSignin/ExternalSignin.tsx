aa703e940c9d5f3d82a0a733200fcc9699b91ed1
Marcos Lopez <marcoslopezdev18@gmail.com>
2024-08-30 02:30:28 -0300

    feat: add google,apple,meta icon in external signin btn

--- frontend/src/app/(auth)/login/_components/ExternalSignin/ExternalSignin.tsx
+++ frontend/src/app/(auth)/login/_components/ExternalSignin/ExternalSignin.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Button from '@/components/core/Button/Button';
 
 const ExternalSignin = () => {
@@ -6,14 +7,59 @@ const ExternalSignin = () => {
       className="flex w-full flex-col gap-y-4 pb-3 pt-2"
       aria-label="Otras formas de iniciar sesion"
       tabIndex={6}>
-      <Button type="submit" disabled size="full" variant="outline" tabIndex={7}>
-        Continuar con Google
+      <Button
+        type="submit"
+        disabled
+        size="full"
+        variant="outline"
+        tabIndex={7}
+        className="relative flex items-center justify-center px-6 py-2">
+        <span className="relative w-fit text-center">
+          <Image
+            src="/icons/GoogleIcon.png"
+            alt="Google Icon"
+            width={24}
+            height={24}
+            className="absolute -left-8 top-1/2 -translate-y-1/2"
+          />
+          Continuar con Google
+        </span>
       </Button>
-      <Button type="submit" disabled size="full" variant="outline" tabIndex={8}>
-        Continuar con Apple
+      <Button
+        type="submit"
+        disabled
+        size="full"
+        variant="outline"
+        tabIndex={8}
+        className="relative flex items-center justify-center px-6 py-2">
+        <span className="relative w-fit text-center">
+          <Image
+            src="/icons/AppleIcon.png"
+            alt="Apple Icon"
+            width={24}
+            height={24}
+            className="absolute -left-8 top-1/2 -translate-y-1/2"
+          />
+          Continuar con Apple
+        </span>
       </Button>
-      <Button type="submit" disabled size="full" variant="outline" tabIndex={9}>
-        Continuar con Meta
+      <Button
+        type="submit"
+        disabled
+        size="full"
+        variant="outline"
+        tabIndex={9}
+        className="relative flex items-center justify-center px-6 py-2">
+        <span className="relative w-fit text-center">
+          <Image
+            src="/icons/MetaIcon.png"
+            alt="Meta Icon"
+            width={24}
+            height={24}
+            className="absolute -left-8 top-1/2 -translate-y-1/2"
+          />
+          Continuar con Apple
+        </span>
       </Button>
     </div>
   );
