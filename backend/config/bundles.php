0fc517628391ab76d4f0385965ad18b7570cd5c7
Nicolas <nicolasgalarza40@gmail.com>
2024-08-29 22:45:26 -0300

    feat: add foundy to make fake data

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -5,4 +5,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
