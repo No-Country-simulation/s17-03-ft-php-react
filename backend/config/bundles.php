20ddc1feec55953edbfdfec66e49c7b858f83dee
Nicolas <nicolasgalarza40@gmail.com>
2024-08-29 22:49:35 -0300

    feat: add fixture bundle to load fake data

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -6,4 +6,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
