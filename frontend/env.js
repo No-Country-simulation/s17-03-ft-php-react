7e03b988351c6294dbe87c9a779b8797cc63d5d6
Brian Tarqui <tarquibrian@gmail.com>
2024-09-03 20:03:47 -0400

    feat: add api url enviroment

--- frontend/env.js
+++ frontend/env.js
@@ -13,9 +13,12 @@ export const env = createEnv({
       .transform(value => parseInt(value, 10))
       .default('3000'),
   },
-  client: {},
+  shared: {
+    API_URL: z.string().url().default('http://localhost:3000'),
+  },
   runtimeEnv: {
     ANALYZE: process.env.ANALYZE,
     PORT: process.env.PORT,
+    API_URL: process.env.API_URL,
   },
 });
